5bf74eddd5b647e0aa6b1220e594107a5e1ee771
rishabh <rishabhjaiswal.1402@gmail.com>
2017-10-14 17:06:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from "./components/todo"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id: 1,name: "A", isComplete: true},
+        {id: 2,name: "B", isComplete: false},
+        {id: 3,name: "C", isComplete: false}
+      ],
+      currentTodo: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+
+        <div className= "Todo-App">
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}/>
+            <TodoList todos={this.state.todos}/>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
